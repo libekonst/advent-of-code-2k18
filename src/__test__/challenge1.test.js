792cb29132ce29943a13e67447f62d98dfabfccc
Kostas Liberopoulos <libekonst@gmail.com>
2018-12-17 18:22:01 +0200

    Separate into smaller functions. Add tests.

--- src/__test__/challenge1.test.js
+++ src/__test__/challenge1.test.js
@@ -1,4 +1,8 @@
-const { sumStringOfNums } = require('../lib/sumStringOfNums')
+const {
+  sumStringOfNums,
+  deserializeNumInput,
+  summarize
+} = require('../lib/sumStringOfNums')
 
 describe('reduces a string of numbers to a sum ', () => {
   test('adds "+1+2" to be 3', () => {
@@ -16,4 +20,12 @@ describe('reduces a string of numbers to a sum ', () => {
   test('ignores the first sequence of digits that don\'t start with plus, causing "1+2" to be 2', () => {
     expect(sumStringOfNums('1+2')).toBe(2)
   })
+})
+
+test('reduces an array of numbers to a sum, making [1,2,3] to equal 6', () => {
+  expect(summarize([1, 2, 3])).toBe(6)
+})
+
+test('decodes a string of numeric inputs into an array of numbers, causing "+1,+2,-3" to equal [1, 2, -3]', () => {
+  expect(deserializeNumInput('+1+2-3')).toEqual([+1, +2, -3])
 })
--- src/lib/sumStringOfNums.js
+++ src/lib/sumStringOfNums.js
@@ -1,8 +1,22 @@
-const sumStringOfNums = string =>
-  string
-    .match(/\++\d*|-+\d*/g)
-    .map(i => parseInt(i))
-    .reduce((sum, next) => sum + next)
+// const sumStringOfNums = string =>
+//   string
+//     .match(/\++\d*|-+\d*/g)
+//     .map(i => parseInt(i))
+//     .reduce((sum, next) => sum + next)
 
-module.exports = { sumStringOfNums }
+/**@param {string} string Serialized numeric values into a string.
+ * @returns {number[]}
+ */
+const deserializeNumInput = string =>
+  string.match(/\++\d*|-+\d*/g).map(val => parseInt(val))
+
+/**@param {number[]} arrayOfNums
+ * @returns {number}
+ */
+const summarize = arrayOfNums => arrayOfNums.reduce((sum, next) => sum + next)
+
+const sumStringOfNums = serializedInput =>
+  summarize(deserializeNumInput(serializedInput))
+
+module.exports = { sumStringOfNums, deserializeNumInput, summarize }
 //TODO: Include leading digits that don't start with a plus sign.

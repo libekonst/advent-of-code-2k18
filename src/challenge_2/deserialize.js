6d67d5b0bb696aeafa4e760e36ba7db32f2499f5
Kostas Liberopoulos <libekonst@gmail.com>
2018-12-28 21:51:25 +0200

    Update deserialize.js

--- src/challenge_2/deserialize.js
+++ src/challenge_2/deserialize.js
@@ -1,26 +1,17 @@
-/**@param {string} string A string of concatenated IDs.
+/**@param {string} stringOfIds A string of concatenated same-length IDs.
  * @param {number} idLength
  * @param {boolean} hasLineBreak Whether the IDs are separated by linebreak '\n' chars.
  * @returns {string[]} An array of all the IDs.
  */
-const deserialize = (string, idLength, hasLineBreak = true) => {
+const deserialize = (stringOfIds, idLength, hasLineBreak = true) => {
   // +1 to skip over the '\n' linebreak char inserted by the template literal
-  // const step = hasLineBreak ? idLength + 1 : idLength
-  const step = (hasLineBreak && 1) + idLength
-  const numberOfIds = string.length / step
+  const step = hasLineBreak ? idLength + 1 : idLength
   let arrayOfIds = []
 
-  for (let i = 0; i < numberOfIds; i++) {
-    const start = i * step
-    const finish = start + idLength
-    const id = string.slice(start, finish)
+  for (let i = 0; i < stringOfIds.length; i += step) {
+    let id = stringOfIds.slice(i, i + idLength)
     arrayOfIds.push(id)
   }
-
-  // for (let i = 0; i < string.length; i += step) {
-  //   let id = string.slice(i, i + idLength)
-  //   array.push(id)
-  // }
 
   return arrayOfIds
 }

69698957b2b16f43d38fd15cb89b59250ccf84cb
Kostas Liberopoulos <libekonst@gmail.com>
2019-01-03 22:59:42 +0200

    Update parse input test.

--- src/__tests__/parseInput.test.js
+++ src/__tests__/parseInput.test.js
@@ -1,18 +1,18 @@
-const deserialize = require('../deserialize')
+const parseInput = require('../parseInput')
 describe('It splits a multiline string into an array of strings by linebreak and whitespace', () => {
   test('transforms "111\\n222\\n333ab\\n" to ["111","222","333ab"]', () => {
-    expect(deserialize('111\n222\n333ab\n')).toEqual(['111', '222', '333ab'])
+    expect(parseInput('111\n222\n333ab\n')).toEqual(['111', '222', '333ab'])
   })
 
   test('transforms "111\\n222\\n333" to ["111","222","333"]', () => {
-    expect(deserialize('111\n222\n333')).toEqual(['111', '222', '333'])
+    expect(parseInput('111\n222\n333')).toEqual(['111', '222', '333'])
   })
 
   test('transforms "111 222 333 " to ["111","222","333"]', () => {
-    expect(deserialize('111 222 333 ')).toEqual(['111', '222', '333'])
+    expect(parseInput('111 222 333 ')).toEqual(['111', '222', '333'])
   })
 
   test('includes both linebreak and whitespace, transforming "aba\\ncs2 aaejk \\n" to ["aba","cs2","aaejk"]', () => {
-    expect(deserialize('aba\ncs2 aaejk \n')).toEqual(['aba', 'cs2', 'aaejk'])
+    expect(parseInput('aba\ncs2 aaejk \n')).toEqual(['aba', 'cs2', 'aaejk'])
   })
 })

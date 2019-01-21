592e8c23f8c61459e2acd2b96423e7177b1ae5ba
Kostas Liberopoulos <libekonst@gmail.com>
2019-01-21 16:50:31 +0200

    Update exact.ts

--- src/challenge_2/exact.ts
+++ src/challenge_2/exact.ts
@@ -8,7 +8,8 @@ export const hasExactly = (intersection: number[]) => (word: string): IExact => 
   const frequency = getLetterFrequencyTracker(word);
   const occurances = Object.values(frequency);
 
-  return intersection.reduce((acc, num) => {
-    return { ...acc, [num]: occurances.includes(num) || false };
-  }, {});
+  return intersection.reduce(
+    (acc, num) => ({ ...acc, [num]: occurances.includes(num) }),
+    {},
+  );
 };

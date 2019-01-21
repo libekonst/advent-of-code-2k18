2bb67dd83b072a60263673d8d7184d543e0728a5
Kostas Liberopoulos <libekonst@gmail.com>
2019-01-21 06:48:17 +0200

    Update duplicatesCounter to be dynamic.

--- src/challenge_2/checksum.ts
+++ src/challenge_2/checksum.ts
@@ -1,18 +1,28 @@
 import { hasExactly, IExact } from './exact';
 import { ITracker } from './frequency';
-
-const duplicatesCounter = (arrayOfInputs: string[]): ITracker => {
-  return arrayOfInputs.map(hasExactly([2, 3])).reduce(updateCounter, { '2': 0, '3': 0 });
-};
-
-export const updateCounter = (counter: ITracker, tracker: IExact): ITracker => ({
-  3: tracker['3'] ? counter['3'] + 1 : counter['3'],
-  2: tracker['2'] ? counter['2'] + 1 : counter['2'],
-});
-
 /** Calculates a checksum by mutliplying the counter's values. */
-const getChecksum = (counter: ITracker): number => {
+export const getChecksum = (counter: ITracker): number => {
   return Object.values(counter).reduce((a, b) => a * b, 1);
 };
 
-export { duplicatesCounter, getChecksum };
+export const duplicatesCounter = (arrayOfInputs: string[], duplicates: number[]) => {
+  return arrayOfInputs
+    .map(hasExactly(duplicates))
+    .reduce(updateCounter, getTrackerFromArray(duplicates));
+};
+
+const updateCounter = (counter: ITracker, tracker: IExact): ITracker => {
+  const keys = Object.keys(counter);
+  const newTracker = keys.reduce((acc, next) => {
+    return { ...acc, [next]: tracker[next] ? acc[next] + 1 : acc[next] };
+  }, counter);
+
+  return newTracker;
+};
+// return (counter: ITracker, tracker: IExact): ITracker => ({
+//   3: tracker['3'] ? counter['3'] + 1 : counter['3'],
+//   2: tracker['2'] ? counter['2'] + 1 : counter['2'],
+// });
+
+const getTrackerFromArray = (arr: any[], init = 0): ITracker =>
+  arr.reduce((tracker, value) => ({ ...tracker, [value]: init }), {});
--- src/challenge_2/index.ts
+++ src/challenge_2/index.ts
@@ -5,6 +5,6 @@ import input from './input';
 // import { ICounter } from './reducers';
 
 const data = parseInput(input);
-const counter: ITracker = duplicatesCounter(data);
+const counter: ITracker = duplicatesCounter(data, [2, 3]);
 
 export default getChecksum(counter);

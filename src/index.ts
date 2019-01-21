efcc0197ac744f2fcab74fcd2594e8f6c92444a6
Kostas Liberopoulos <libekonst@gmail.com>
2019-01-21 07:08:52 +0200

    Update to print a table to the console.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 /* eslint-disable no-console */
 import { part1 as challenge1A, part2 as challenge1B } from './challenge_1';
+import { part1 as challenge2A } from './challenge_2';
 import challenge2 = require('./challenge_2');
 
-const answers = {
+const answers: any = {
   'Day 1a': challenge1A,
   'Day 1b': challenge1B,
-  'Day 2': challenge2,
-  // 'Day 3': null,
+  'Day 2': challenge2A,
 };
 
-console.log(answers);
+const table =
+  'ANSWERS:' +
+  Object.keys(answers)
+    .map(k => `\n${k}: ${answers[k]}`)
+    .join('');
+
+console.log(table);

5d4e91cda510934253a49dde4ed00962d01a19bc
Kostas Liberopoulos <libekonst@gmail.com>
2019-01-21 07:09:30 +0200

    Manage exports.

--- src/challenge_2/index.ts
+++ src/challenge_2/index.ts
@@ -1,10 +1,8 @@
 import parseInput from '../parseInput';
 import { duplicatesCounter, getChecksum } from './checksum';
-import { ITracker } from './frequency';
 import input from './input';
 // import { ICounter } from './reducers';
 
 const data = parseInput(input);
-const counter: ITracker = duplicatesCounter(data, [2, 3]);
-
-export default getChecksum(counter);
+export const part1 = getChecksum(duplicatesCounter(data, [2, 3]));
+// export default getChecksum(duplicatesCounter(data, [2, 3]));

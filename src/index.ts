900e216b6527f9e66f18c3c02e6ef23c33afd055
Kostas Liberopoulos <libekonst@gmail.com>
2019-01-24 05:59:21 +0200

    Tux.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 import { part1 as challenge1A, part2 as challenge1B } from './challenge_1';
 import { part1 as challenge2A, part2 as challenge2B } from './challenge_2';
-import challenge2 = require('./challenge_2');
 
 const answers: any = {
   'Day 1A': challenge1A,
@@ -10,10 +9,29 @@ const answers: any = {
   'Day 2B': challenge2B,
 };
 
+const tux = `
+ ------------------
+< You are awesome! >
+ ------------------
+   \\
+    \\
+        .--.
+       |o_o |
+       |:_/ |
+      //   \\ \\
+     (|     | )
+    /'\\_   _/'\\
+    \\___)=(___/
+`;
+
 const table =
-  'ANSWERS:' +
+  tux +
+  `\n~${new Date().toLocaleTimeString()}` +
+  '\n\n       ANSWERS' +
+  '\n=====================\n' +
   Object.keys(answers)
-    .map(k => `\n${k}: ${answers[k]}`)
+    .map(k => `  ${k}  |  ${answers[k]}\n`)
     .join('');
 
 console.log(table);
+// console.table(answers);

5b5c4cf1962c66d238a71362d1a279c7f65a5f4f
Kostas Liberopoulos <libekonst@gmail.com>
2019-01-26 09:39:46 +0200

    Update parseInput.ts

--- src/parseInput.ts
+++ src/parseInput.ts
@@ -1,4 +1,4 @@
-const parseInput = (input: string): string[] =>
-  input.split(/\n|\s/).filter(val => val !== '');
+/** Splits concatenated string input into a `string[]` by linebreak and space. */
+const parseInput = (input: string) => input.split(/\n|\s/).filter(val => val !== '');
 
 export default parseInput;
